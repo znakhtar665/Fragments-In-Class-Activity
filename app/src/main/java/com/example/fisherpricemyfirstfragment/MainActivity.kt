06dc974dbd7748f3df61fdc118d36d296af0436e
znakhtar665 <znakhtar665@gmail.com>
2024-10-10 15:24:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fisherpricemyfirstfragment/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.fisherpricemyfirstfragment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView2, ColorFragment())
+            .commit()
+
+    }
+}
